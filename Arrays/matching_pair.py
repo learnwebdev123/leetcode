555bc62145e95a1c27e9d979089a0b28efa8b9f9
Sivaram Yarasuri <sivaramyarasuri@gmail.com>
2022-12-13 21:47:32 +0530

    code logic for matching pair

--- Arrays/matching_pair.py
+++ Arrays/matching_pair.py
@@ -1,2 +1,12 @@
 class Solution:
-    pass
+    
+    def solve(self,a ,b):
+        n = len(a)
+        for i in range(0, n-1):
+            for j in range(i+1,n):
+                if(a[i]+a[j] == b):
+                    return True
+        return False
+
+obj = Solution()
+print(obj.solve([1,2,3,4],7))
