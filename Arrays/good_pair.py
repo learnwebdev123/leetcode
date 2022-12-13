cc75b65785d52f2f17b3a47192b3900ddb753d52
Sivaram Yarasuri <sivaramyarasuri@gmail.com>
2022-12-13 21:16:11 +0530

    Good pair problem

--- Arrays/good_pair.py
+++ Arrays/good_pair.py
@@ -1,12 +1,1 @@
-class Solution:
 
-    def solve(self, a,b):
-        for i in range(0,len(a)-1):
-            for j in range(i+1,len(a)):
-                if (a[i]+a[j] == b):
-                    return True
-        return False
-
-
-obj = Solution()
-print(obj.solve(1,2,3,4),7)
